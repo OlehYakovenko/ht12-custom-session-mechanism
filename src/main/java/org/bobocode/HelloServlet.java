574785c84d7396e5853dafc46c5272d336b7484c
oleg.iakovenko <iakovenkoua@gmail.com>
2023-11-02 11:37:17 +0200

    Implement first part of training.Use Servlet session for save users name.

---
+++ src/main/java/org/bobocode/HelloServlet.java
@@ -0,0 +1,41 @@
+package org.bobocode;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import javax.print.attribute.standard.MediaSize;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/hello")
+public class HelloServlet extends HttpServlet {
+    private static final String NAME = "name";
+    private static final String HELLO = "Hello";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var writer = new PrintWriter(resp.getOutputStream());
+        writer.println(getMessage(req));
+        writer.flush();
+    }
+
+    private String getMessage(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (request.getParameter(NAME) == null && session.getAttribute(NAME) != null) {
+            return getMessageFormatted(session);
+        } else if (request.getParameter(NAME) != null) {
+            session.setAttribute(NAME, request.getParameter(NAME));
+            return getMessageFormatted(session);
+        }
+        return HELLO;
+    }
+
+    private static String getMessageFormatted(HttpSession session) {
+        return """
+                %s %s!""".formatted(HELLO, session.getAttribute(NAME));
+    }
+}
