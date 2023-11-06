125ab49d562b9225d710f0e142ae351f6aa55f0a
oleg.iakovenko <iakovenkoua@gmail.com>
2023-11-06 16:06:17 +0200

    Create Session and CustomSession

--- src/main/java/org/bobocode/Session.java
+++ src/main/java/org/bobocode/Session.java
@@ -1,15 +1,13 @@
 package org.bobocode;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 public class Session {
     public final static String SESSION_ID = "CUSTOMSESSIONID";
     @Setter
-    @Getter
     private String name;
-    @Getter
     private final String sessionId;
 
     public Session(String sessionId, String name) {
